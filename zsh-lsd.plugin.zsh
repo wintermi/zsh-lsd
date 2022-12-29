57b4888e2f024ae551025aae5c03035e11b8915e
Matthew Winter <33818+wintermi@users.noreply.github.com>
2022-12-29 21:13:30 +1100

    fix: recursive depth

--- zsh-lsd.plugin.zsh
+++ zsh-lsd.plugin.zsh
@@ -4,5 +4,5 @@ if command -v lsd &> /dev/null; then
   alias ls='lsd --group-directories-first'
   alias ll='lsd -l --group-directories-first'
   alias la='lsd -la --group-directories-first'
-  alias tree='lsd -l --group-directories-first --tree --level=2'
+  alias tree='lsd -l --group-directories-first --tree --depth=2'
 fi
