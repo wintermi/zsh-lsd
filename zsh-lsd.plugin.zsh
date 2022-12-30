a1dd569a065e6e8b037921adc9a93f7e08c69b83
Matthew Winter <33818+wintermi@users.noreply.github.com>
2022-12-30 18:49:50 +1100

    feat: Output error message when 'lsd' command not found

--- zsh-lsd.plugin.zsh
+++ zsh-lsd.plugin.zsh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-if command -v lsd &> /dev/null; then
-  alias ls='lsd --group-directories-first'
-  alias ll='lsd -l --group-directories-first'
-  alias la='lsd -la --group-directories-first'
-  alias tree='lsd -l --group-directories-first --tree --depth=2'
+# Exit if the 'lsd' command could not be found
+if ! (( $+commands[lsd] )); then
+    echo "ERROR: 'lsd' command not found"
+    return
 fi
+
+# Create alias override commands using 'lsd'
+alias ls='lsd --group-directories-first'
+alias ll='lsd -l --group-directories-first'
+alias la='lsd -la --group-directories-first'
+alias tree='lsd -l --group-directories-first --tree --depth=2'
